8b2a899ab38dbb8a7e25292200d744548bca210b
guguangyue <guguangyue@lian-o.com>
2024-08-01 17:15:56 +0800

    UMP Media Player

---
+++ urp_example_learning/Assets/UI/VR面板/MyVideo.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MyVideo : MonoBehaviour
+{
+    public Text text_PlayOrPause;
+    public Button button_PlayOrPause;
+    public Slider slider;
+
+    private VideoPlayer videoPlayer;
+    private RawImage rawImage;
+
+    private int flag = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        rawImage = GetComponent<RawImage>();
+        button_PlayOrPause.onClick.AddListener(PlayorPause);
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayorPause()
+    {
+        if(videoPlayer.isPlaying == true)
+        {
+            videoPlayer.Pause();
+            text_PlayOrPause.text = "²¥·Å";
+            Time.timeScale = 0;
+        }
+        else
+        {
+            videoPlayer.Play();
+            text_PlayOrPause.text = "ÔÝÍ£";
+        }
+    }
+
+    void OnSliderValueChanged(float value)
+    {
+        videoPlayer.time = videoPlayer.length * value;
+    }
+}
